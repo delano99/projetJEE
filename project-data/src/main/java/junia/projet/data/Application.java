61cdbf336f4e2dacc851da0fe29f592ac8df14d6
delano99 <delanonyotue@gmail.com>
2022-02-19 19:21:22 +0100

    create variable and registrer in data base

--- project-data/src/main/java/junia/projet/data/Application.java
+++ project-data/src/main/java/junia/projet/data/Application.java
@@ -9,6 +9,7 @@ import junia.core.service.GameRoomService;
 import junia.core.service.JeuService;
 import junia.core.service.SessionService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.apache.log4j.BasicConfigurator;
 
 import java.sql.Time;
 import java.util.HashMap;
@@ -38,13 +39,113 @@ public class Application {
         jeuService.deleteAll();
     }
 
+    private static Map<String, Arcade> registerArcade(ArcadeService arcadeService) {
+
+        Map<String, Arcade> arcadeMap = new HashMap<>();
+        arcadeMap.put("PONG", createArcade("PONG", arcadeService));
+        arcadeMap.put("Galaga", createArcade("Galaga", arcadeService));
+        arcadeMap.put("Gradius", createArcade("Gradius", arcadeService));
+        return arcadeMap;
+    }
+
+    private static Arcade createArcade(String arcadeName, final ArcadeService arcadeService) {
+
+        Arcade arcade = new Arcade();
+        arcade.setArcadeName(arcadeName);
+
+        arcadeService.save(arcade);
+        return arcade;
+    }
+
+
     private static Map<String, Session> registerSession(SessionService sessionService, Map<String, Jeu> jeuMap, Map<String, GameRoom> gameRoomMap) {
+
+        Map<String, Session> sessions = new HashMap<>();
+        java.sql.Time timeMorning = new Time(8, 00, 00);
+        java.sql.Time timeNoon = new Time(12, 00, 00);
+        java.sql.Time timeAfterNoon = new Time(16, 00, 00);
+        java.sql.Time timeEvening = new Time(20, 00, 00);
+        String str = "2015-03-31";
+        java.sql.Date date = java.sql.Date.valueOf(str);//converting string into sql date
+
+        sessions.put("Session 1", createSession(jeuMap.get("The Legend of Zelda: Ocarina of Time"), gameRoomMap.get("Room 1 PONG"), date, timeNoon, sessionService));
+        sessions.put("Session 2", createSession(jeuMap.get("Metroid Dread"), gameRoomMap.get("Room 1 PONG"), date, timeNoon, sessionService));
+        sessions.put("Session 3", createSession(jeuMap.get("The Legend of Zelda: Ocarina of Time"), gameRoomMap.get("Room 1 PONG"), date, timeAfterNoon, sessionService));
+        sessions.put("Session 4", createSession(jeuMap.get("PResident Evil Village"), gameRoomMap.get("Room 1 PONG"), date, timeEvening, sessionService));
+        sessions.put("Session 5", createSession(jeuMap.get("Psychonauts 2 "), gameRoomMap.get("Room 2 PONG"), date, timeNoon, sessionService));
+        sessions.put("Session 6", createSession(jeuMap.get("The Legend of Zelda: Ocarina of Time"), gameRoomMap.get("Room 2 PONG"), date, timeNoon, sessionService));
+        sessions.put("Session 7", createSession(jeuMap.get("Super Mario 3D World + Bowser's Fury"), gameRoomMap.get("Room 2 PONG"), date, timeAfterNoon, sessionService));
+        sessions.put("Session 8", createSession(jeuMap.get("Metroid Dread"), gameRoomMap.get("Room 2 PONG"), date, timeEvening, sessionService));
+        sessions.put("Session 9", createSession(jeuMap.get("PResident Evil Village"), gameRoomMap.get("Room 3 PONG"), date, timeNoon, sessionService));
+        sessions.put("Session 10", createSession(jeuMap.get("PResident Evil Village"), gameRoomMap.get("Room 3 PONG"), date, timeNoon, sessionService));
+        sessions.put("Session 11", createSession(jeuMap.get("Super Mario 3D World + Bowser's Fury"), gameRoomMap.get("Room 3 PONG"), date, timeAfterNoon, sessionService));
+        sessions.put("Session 12", createSession(jeuMap.get("The Legend of Zelda: Ocarina of Time"), gameRoomMap.get("Room 3 PONG"), date, timeEvening, sessionService));
+
+
+        sessions.put("Session 13", createSession(jeuMap.get("The Legend of Zelda: Ocarina of Time"), gameRoomMap.get("Room 1 Galaga"), date, timeNoon, sessionService));
+        sessions.put("Session 14", createSession(jeuMap.get("PResident Evil Village"), gameRoomMap.get("Room 1 Galaga"), date, timeNoon, sessionService));
+        sessions.put("Session 15", createSession(jeuMap.get("The Legend of Zelda: Ocarina of Time"), gameRoomMap.get("Room 1 Galaga"), date, timeAfterNoon, sessionService));
+        sessions.put("Session 16", createSession(jeuMap.get("Metroid Dread"), gameRoomMap.get("Room 1 Galaga"), date, timeEvening, sessionService));
+        sessions.put("Session 17", createSession(jeuMap.get("Metroid Dread"), gameRoomMap.get("Room 2 Galaga"), date, timeNoon, sessionService));
+        sessions.put("Session 18", createSession(jeuMap.get("Psychonauts 2 "), gameRoomMap.get("Room 2 Galaga"), date, timeNoon, sessionService));
+        sessions.put("Session 19", createSession(jeuMap.get("Psychonauts 2 "), gameRoomMap.get("Room 2 Galaga"), date, timeAfterNoon, sessionService));
+        sessions.put("Session 20", createSession(jeuMap.get("Super Mario 3D World + Bowser's Fury"), gameRoomMap.get("Room 2 Galaga"), date, timeEvening, sessionService));
+        sessions.put("Session 21", createSession(jeuMap.get("Metroid Dread"), gameRoomMap.get("Room 3 Galaga"), date, timeNoon, sessionService));
+        sessions.put("Session 22", createSession(jeuMap.get("Super Mario 3D World + Bowser's Fury"), gameRoomMap.get("Room 3 Galaga"), date, timeNoon, sessionService));
+        sessions.put("Session 23", createSession(jeuMap.get("Metroid Dread"), gameRoomMap.get("Room 3 Galaga"), date, timeAfterNoon, sessionService));
+        sessions.put("Session 24", createSession(jeuMap.get("Super Mario 3D World + Bowser's Fury"), gameRoomMap.get("Room 3 Galaga"), date, timeEvening, sessionService));
+
+        sessions.put("Session 25", createSession(jeuMap.get("Metroid Dread"), gameRoomMap.get("Room 1 Gradius"), date, timeNoon, sessionService));
+        sessions.put("Session 26", createSession(jeuMap.get("Metroid Dread"), gameRoomMap.get("Room 1 Gradius"), date, timeNoon, sessionService));
+        sessions.put("Session 27", createSession(jeuMap.get("Super Mario 3D World + Bowser's Fury"), gameRoomMap.get("Room 1 Gradius"), date, timeAfterNoon, sessionService));
+        sessions.put("Session 28", createSession(jeuMap.get("Psychonauts 2"), gameRoomMap.get("Room 1 Gradius"), date, timeEvening, sessionService));
+        sessions.put("Session 29", createSession(jeuMap.get("Psychonauts 2"), gameRoomMap.get("Room 2 Gradius"), date, timeNoon, sessionService));
+        sessions.put("Session 30", createSession(jeuMap.get("Super Mario 3D World + Bowser's Fury"), gameRoomMap.get("Room 2 Gradius"), date, timeNoon, sessionService));
+        sessions.put("Session 31", createSession(jeuMap.get("Super Mario 3D World + Bowser's Fury"), gameRoomMap.get("Room 2 Gradius"), date, timeAfterNoon, sessionService));
+        sessions.put("Session 32", createSession(jeuMap.get("Psychonauts 2"), gameRoomMap.get("Room 2 Gradius"), date, timeEvening, sessionService));
+        sessions.put("Session 33", createSession(jeuMap.get("PResident Evil Village"), gameRoomMap.get("Room 3 Gradius"), date, timeNoon, sessionService));
+        sessions.put("Session 34", createSession(jeuMap.get("PResident Evil Village"), gameRoomMap.get("Room 3 Gradius"), date, timeNoon, sessionService));
+        sessions.put("Session 35", createSession(jeuMap.get("Metroid Dread"), gameRoomMap.get("Room 3 Gradius"), date, timeAfterNoon, sessionService));
+        sessions.put("Session 36", createSession(jeuMap.get("The Legend of Zelda: Ocarina of Time"), gameRoomMap.get("Room 3 Gradius"), date, timeEvening, sessionService));
+
+        return sessions;
+    }
+
+    private static Session createSession(Jeu jeu, GameRoom gameRoom, java.sql.Date date, java.sql.Time startHour, final SessionService sessionService) {
+
+        Session session = new Session();
+        session.setJeu(jeu);
+        session.setStartHour(startHour);
+        session.setDate(date);
+        session.setGameRoom(gameRoom);
+
+        sessionService.save(session);
+        return session;
     }
 
     private static Map<String, GameRoom> registerGameRoom(GameRoomService gameRoomService, Map<String, Arcade> arcadeMap) {
+
+        Map<String, GameRoom> gameRoomMap = new HashMap<>();
+        gameRoomMap.put("Room 1 PONG", createGameRoom(1, arcadeMap.get("PONG"), gameRoomService));
+        gameRoomMap.put("Room 2 PONG", createGameRoom(2, arcadeMap.get("PONG"), gameRoomService));
+        gameRoomMap.put("Room 3 PONG", createGameRoom(3, arcadeMap.get("PONG"), gameRoomService));
+        gameRoomMap.put("Room 1 Galaga", createGameRoom(1, arcadeMap.get("Galaga"), gameRoomService));
+        gameRoomMap.put("Room 2 Galaga", createGameRoom(2, arcadeMap.get("Galaga"), gameRoomService));
+        gameRoomMap.put("Room 3 Galaga", createGameRoom(3, arcadeMap.get("Galaga"), gameRoomService));
+        gameRoomMap.put("Room 1 Gradius", createGameRoom(1, arcadeMap.get("Gradius"), gameRoomService));
+        gameRoomMap.put("Room 2 Gradius", createGameRoom(2, arcadeMap.get("Gradius"), gameRoomService));
+        gameRoomMap.put("Room 3 Gradius", createGameRoom(3, arcadeMap.get("Gradius"), gameRoomService));
+        return gameRoomMap;
     }
 
-    private static Map<String, Arcade> registerArcade(ArcadeService arcadeService) {
+    private static GameRoom createGameRoom(Integer gameRoomNumber, Arcade arcade, final GameRoomService gameRoomService) {
+
+        GameRoom gameRoom = new GameRoom();
+        gameRoom.setRoomNumber(gameRoomNumber);
+        gameRoom.setArcade(arcade);
+
+        gameRoomService.save(gameRoom);
+        return gameRoom;
     }
 
     private static Map<String, Jeu> registerJeu(JeuService jeuService) {
